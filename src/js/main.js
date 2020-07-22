baefa53d61f149bd59a99015e78a42281a6e7e43
batman <ITuser@flyup.tw>
2020-07-22 17:59:25 +0800

    add webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,67 @@
 import * as PIXI from 'pixi.js'
+import axios from 'axios'
 
 const app = new PIXI.Application({
-  width: 720,
-  height: 1280,
-  backgroundColor: 0x1099bb,
+  width: 800,
+  height: 600,
   view: document.querySelector('#scene'),
-  resolution: window.devicePixelRatio || 1
+  resolution: 1
+  // resolution: window.devicePixelRatio || 1
 });
+// //////////////////////////////////////////////
+const stage = new PIXI.Container();
+const loader = new PIXI.Loader();
 
-const texture = PIXI.Texture.from('assets/bunny.png');
+// //////////////////////////////////////////////
+// 資源載入器
+const resourcesList= [
+  'assets/img/flowerTop.png',
+  'assets/img/bunny.png'
+]
+resourcesList.map(element => {
+  loader.add(element);
+});
+loader.load(init)
+loader.onLoad.add(() => loadHandler()); 
+loader.onProgress.add((event)=>loadProgressHandler(event));
+loader.onComplete.add(() => allComplete());
+// //////////////////////////////////////////////
+
+const texture = PIXI.Texture.from(resourcesList[1]);
 const bunny = new PIXI.Sprite(texture);
 bunny.anchor.set(0.5);
 bunny.x = 160
 bunny.y = 160
 app.stage.addChild(bunny);
 
-app.ticker.add((delta) => {
-  bunny.rotation -= 0.01 * delta;
-});
+// //////////////////////////////////////////////
+function loadHandler() {
+  console.log('loadHandler');
+}
+function loadProgressHandler(event) {
+  console.log(resourcesList.length);
+  console.log('loadProgressHandler: '+event.progress + '%');
+}
+function init() {
+  console.log('init');
+}
+function allComplete() {
+  axios.get('https://t-api.tgcity18.info/api/Home/web')
+  .then((res)=>{
+    console.log(res)
+  })
+  console.log('allComplete');
+  animate();
+}
+// //////////////////////////////////////////////
+function animate() {
+  requestAnimationFrame(animate);
+  bunny.rotation -= 0.01;
+  app.render(stage);
+}
+animate();
+
+
+// app.ticker.add((delta) => {
+//   bunny.rotation -= 0.01 * delta;
+// });
--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ImageminPlugin = require('imagemin-webpack-plugin').default
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+// const YYYY = new Date().getFullYear().toString();
+// const MM = ((new Date().getMonth() + 1) < 10 ? `0${new Date().getMonth() + 1}` : `${(new Date().getMonth() + 1)}`);
+// const DD = ((new Date().getDate()) < 10 ? `0${new Date().getDate()}` : `${(new Date().getDate())}`);
+// const HH = ((new Date().getHours()) < 10 ? `0${new Date().getHours()}` : `${(new Date().getHours())}`);
+// const mm = ((new Date().getMinutes()) < 10 ? `0${new Date().getMinutes()}` : `${(new Date().getMinutes())}`);
+// process.env.BUILDTIME = `${YYYY}${MM}${DD}${HH}${mm}`;
+// process.env.VERSION = (process.env.VER + process.env.BUILDTIME).toString();
+
 module.exports = {
   context: path.join(__dirname, 'src'),
   entry: ['./js/main.js'],
   // mode: 'none', // none development production
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'oputput'),
     filename: 'game.min.[hash:8].js',
   },
   target: 'web',
