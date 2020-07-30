e2557232660d15d0e83640168125ceca5bf5bcab
W <w@gmail.com>
2020-07-30 18:24:50 +0800

    oop update

--- src/Screen/Loading.js
+++ src/Screen/Loading.js
@@ -1,14 +1,47 @@
 import * as PIXI from 'pixi.js';
 
 export default class Loading extends PIXI.Container {
-  init() {
-    this.redBG = new PIXI.Graphics();
-    this.redBG .beginFill(0xff0000);
-    this.redBG .drawRect(0, 0, 300, 300);
-    this.addChild(this.redBG);
+  constructor() {
+    super();
   }
-  setLoading(num){
-    // console.log(num);
-    this.redBG.width = 300 * (num/ 100);
+
+  init() {
+    this.WW = window.innerWidth;
+    this.HH = window.innerHeight;
+    this.LoadingBar = new PIXI.Container();
+    this.LoadingBarBg = new PIXI.Sprite(PIXI.Texture.WHITE);
+    this.LoadingBarBg.width = this.WW;
+    this.LoadingBarBg.height = 10;
+    this.LoadingBarBg.scale.x = 0.01;
+    this.addChild(this.LoadingBar);
+    this.LoadingBar.addChild(this.LoadingBarBg);
+    this.textItem = new PIXI.Text('');
+    this.textItem.resolution = 2;
+    this.textItem.anchor.set(0.5);
+    this.textItem.style = new PIXI.TextStyle({
+      fill: 0xffffff,
+      fontSize: 16,
+      fontFamily: 'Arial',
+      fontStyle: 'bold',
+    });
+
+    this.addChild(this.textItem);
+  }
+
+  resize() {
+    console.log('resize');
+    this.WW = window.innerWidth;
+    this.HH = window.innerHeight;
+    this.textItem.position.x = Math.floor(this.WW / 2);
+    this.textItem.position.y = Math.floor((this.HH / 2) - 20);
+    this.LoadingBar.position.y = Math.floor(this.HH / 2);
+  }
+  setLoading(num) {
+    this.textItem.text = 'Loading ' + Math.floor(num) + '%';
+    this.LoadingBarBg.width = this.WW;
+    this.LoadingBarBg.scale.x = Math.floor(num);
+  }
+  removed() {
+    console.log('removeEvent');
   }
 }
--- src/app.js
+++ src/app.js
@@ -2,41 +2,53 @@ import * as PIXI from 'pixi.js';
 import Loader from './Loader';
 import Loading from './Screen/Loading';
 import Resouce from "./Resouce";
-
+import {
+  TweenMax
+} from 'gsap';
 
 export default class App {
   constructor() {
     this.app = new PIXI.Application({
-      width: 1440,
-      height: 720,
+      width: 800,
+      height: 600,
+      antialias: false,
       view: document.querySelector('#app'),
       resolution: 1,
     });
+    // //////////////////////////////////////////////////
     this.setup();
+    // //////////////////////////////////////////////////
     this.LoadingScreen = new Loading();
+    this.LoadingScreen.init();
     this.app.stage.addChild(this.LoadingScreen);
-    this.LoadingScreen.init(); 
+
+    window.onresize = () => this.appResize();
+    this.appResize();
   }
 
   setup() {
-    // 
-    this.WW = this.app.view.width;
-    this.HH = this.app.view.height;
-    // 
     this.Loader = PIXI.Loader.shared;
     for (var i in Resouce) {
-      this.Loader.add(i, Resouce[i])
+      this.Loader.add(i, Resouce[i]);
     }
     this.Loader.load();
     this.Loader.onProgress.add((event) => this.getProgress(event));
     this.Loader.onComplete.add(() => this.onAssetsLoaded());
-    
   }
-  getProgress(event){    
-    this.LoadingScreen.setLoading(event.progress)
+  appResize() {
+    app.width = window.innerWidth;
+    app.height = window.innerHeight;
+    if (this.LoadingScreen) this.LoadingScreen.resize();
+  }
+  getProgress(event) {
+    this.LoadingScreen.setLoading(event.progress);
     return event.progress;
   }
-  onAssetsLoaded(){
-    console.log('onAssetsLoaded');
+  onAssetsLoaded() {
+    // this.app.stage.removeChild(this.LoadingScreen);
+    TweenMax.to(this.LoadingScreen, 1, {
+      alpha: 0
+    })
+    // delete this.LoadingScreen;
   }
 }
